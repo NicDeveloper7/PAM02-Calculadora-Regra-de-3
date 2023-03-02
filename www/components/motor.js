0e3e25c81815815370cff195a74156d7ca519af6
NicDeveloper7 <nicholasdev@zohomail.com>
2023-03-03 05:13:22 +0900

    Projeto

--- www/components/motor.js
+++ www/components/motor.js
@@ -1,2 +1,27 @@
-// This is a JavaScript file
+var resultado;
+var resultado1;
+
+function calcularregrade3() {
+
+    var primeirovalor, segundovalor, terceirovalor;
+    
+    primeirovalor = document.getElementById('valor1').value
+    segundovalor = document.getElementById('valor2').value
+    terceirovalor = document.getElementById('valor3').value
+   
+    resultado1 = resultado;
+    
+    resultado = (segundovalor * terceirovalor) / primeirovalor
+
+
+
+    document.getElementById('resultado').value = resultado
+
+    document.getElementById('result').textContent = resultado
+
+    document.getElementById('result1').textContent = resultado1
+
+}
+
+
 
